39a1216c3d0d363f790753fef4e71e5fba6bbce4
Herdez <abelquantikum@gmail.com>
2018-07-22 19:00:24 -0500

    Lab Javascript Fundamentals Capstone

---
+++ ex-04-toCamelCase.js
@@ -0,0 +1,39 @@
+
+/**
+ * toCamelCase()
+ *
+ * Write a `toCamelCase` function that takes a multi word string as an argument
+ * and returns a string that follows camel case convention
+ *
+ */
+
+function toCamelCase(str) {
+	var listOfWords = str.split(" ");
+	for (var i = 1; i < listOfWords.length; i++) {
+		var word = listOfWords[i].toLowerCase();
+		listOfWords[i] = word.replace(word[0], word[0].toUpperCase());
+	}
+	return listOfWords.join("");
+}
+
+
+
+//~+~+~+~+~+~+~+~Don't Touch~+~+~++~+~+~+~+~+~+~+
+console.log("[1] Should return 'fetchRecords'");
+console.assert(toCamelCase("fetch records") === 'fetchRecords')
+console.log("====================================");
+console.log();
+
+console.log("[2] Should return 'createTableRow'");
+console.assert(  toCamelCase("create table row") === 'createTableRow'  )
+console.log("====================================");
+console.log();
+
+console.log("[3] Should return 'convertToHtml'");
+console.assert(  toCamelCase("convert to HTML") === 'convertToHtml'  )
+console.log("====================================");
+console.log();
+
+console.log("[4] Should return 'getUserAccountId'");
+console.assert(  toCamelCase("get user account ID") === 'getUserAccountId'  )
+console.log("====================================");
